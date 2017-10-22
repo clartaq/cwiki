347111a6ebb39b04497eab5ff4338e6b98da17b5
clartaq <david@dneclark.net>
2017-10-22 16:58:14 -0400

    Editing existing pages working. Adding new pages working, but buggy and incomplete.

--- src/cwiki/routes/home.clj
+++ src/cwiki/routes/home.clj
@@ -13,14 +13,39 @@
 (defn read-front-page-content
   "Read in the front page for a new wiki."
   []
-  (:content (read-front-page))) ;find-post-by-title "Front Page"))]
-    ;(println "fp:" fp)
-    ;fp))
+  (:content (read-front-page)))
 
+(defn save-edits
+  [page-id new-title new-content]
+  ;(println "page-id:" page-id ", (type page-id):" (type page-id))
+  (let [actual-id (Integer. ^String page-id)]
+   ; (println "save-edits: actual-id:" actual-id ", new-title:" new-title ",
+   ; new-content:" new-content)
+    (db/update-page-title-and-content actual-id new-title new-content)
+    (layout/view-wiki-page (db/find-post-by-title new-title))))
 
+(defn save-new-page
+  [title content]
+  ;(println "save-new-page: title:" title ", content:" content)
+ ; (println "insert:"
+  (db/insert-new-page! title content)
+  ;)
+  (let [new-view (layout/view-wiki-page (db/find-post-by-title title))]
+  ;  (println "new-view:" new-view)
+    new-view))
 
 (defn home []
-  (layout/view-wiki-page (read-front-page))) ;compose-wiki-page (read-front-page-content)))
+  (layout/view-wiki-page (read-front-page)))
 
 (defroutes home-routes
-           (GET "/" [] (home)))
+           (GET "/" [] (home))
+           (POST "/save-edits" request
+             (let [params (request :multipart-params)]
+               (save-edits (get params "page-id")
+                           (get params "title")
+                           (get params "content"))))
+           (POST "/save-new-page" request
+             (println "request:" request)
+             (let [params (request :multipart-params)]
+               (save-new-page (get params "title")
+                              (get params "content")))))
