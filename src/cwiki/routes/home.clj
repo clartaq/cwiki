66bc258b81838ea093d3260aa2761e891f2b197c
clartaq <david@dneclark.net>
2017-10-29 12:19:16 -0400

    Removed unused code.

--- src/cwiki/routes/home.clj
+++ src/cwiki/routes/home.clj
@@ -14,22 +14,14 @@
 
 (defn save-edits
   [page-id new-title new-content]
-  ;(println "page-id:" page-id ", (type page-id):" (type page-id))
   (let [actual-id (Integer. ^String page-id)]
-   ; (println "save-edits: actual-id:" actual-id ", new-title:" new-title ",
-   ; new-content:" new-content)
     (db/update-page-title-and-content! actual-id new-title new-content)
     (layout/view-wiki-page (db/find-post-by-title new-title))))
 
 (defn save-new-page
   [title content]
-  ;(println "save-new-page: title:" title ", content:" content)
- ; (println "insert:"
   (db/insert-new-page! title content)
-  ;)
-  (let [new-view (layout/view-wiki-page (db/find-post-by-title title))]
-  ;  (println "new-view:" new-view)
-    new-view))
+  (layout/view-wiki-page (db/find-post-by-title title)))
 
 (defn home []
   (layout/view-wiki-page (read-front-page)))
