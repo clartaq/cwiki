5065d534487edec0c1bd65f35b87a18796797ca5
clartaq <clartaq@axaluna.com>
2017-10-29 12:31:27 -0400

    Removed unused and duplicate requires.

--- src/cwiki/handler.clj
+++ src/cwiki/handler.clj
@@ -1,15 +1,12 @@
 (ns cwiki.handler
   (:require [cemerick.url :as u]
             [compojure.core :refer [defroutes routes]]
-            [ring.middleware.resource :refer [wrap-resource]]
-            [ring.middleware.file-info :refer [wrap-file-info]]
             [ring.util.response :refer [status]]
             [hiccup.middleware :refer [wrap-base-url]]
             [compojure.handler :as handler]
             [compojure.route :as route]
             [cwiki.views.layout :as layout]
             [cwiki.routes.home :refer [home-routes]]
-            [compojure.response :as response]
             [compojure.response :as response]
             [clojure.string :as s]
             [cwiki.models.db :as db]))
@@ -32,45 +29,26 @@
     (let [raw-title (u/url-decode (:uri request))
           title (s/replace raw-title "//" "")
           raw-post (db/find-post-by-title title)]
-     ; (println "raw-title:" raw-title ", title:" title ", raw-post:" raw-post)
      (cond
        raw-post (let [new-body (:content raw-post)
-                      new-page (layout/view-wiki-page raw-post)] ;(layout/compose-wiki-page new-body)]
+                      new-page (layout/view-wiki-page raw-post)]
                   (println "Found title:" title)
                   (build-response new-page request))
-       ;(-> (response/render new-page request)
-       ;    (status 200)
-       ;    (assoc :body new-page)))
 
        (s/ends-with? title "/edit") (let [title-only (s/replace title "/edit" "")
-                                          ; _ (println "saw edit request for:" title-only)
                                           new-body (layout/compose-edit-page
                                                      (db/find-post-by-title title-only))]
                                       (build-response new-body request))
-       ;(-> (response/render new-body request)
-       ;    (status 200)
-       ;    (assoc :body new-body)))
        (s/ends-with? title "/delete") (let [title-only (s/replace title "/delete" "")
                                             new-body (layout/view-wiki-page
                                                        (db/find-post-by-title "Front Page"))]
                                         (db/delete-page-by-id (db/title->page-id title-only))
                                         (build-response new-body request))
-       ;(-> (response/render new-body request)
-       ;    (status 200)
-       ;    (assoc :body new-body)))
-       ;(s/ends-with? title "/create")
        :else (let [title-only (s/replace title "/create" "")
-                   ;  _ (println "saw create request for:" title-only)
                    new-body (layout/compose-create-page
                               (db/create-new-post-map title-only))]
-               ;(println "new-body:" new-body)
                (build-response new-body request))
-       ;(-> (response/render new-body request)
-       ;    (status 200)
-       ;    (assoc :body new-body)))
        ))))
-
-     ;   :else nil))))
 
 (defroutes app-routes
            (route/resources "/")
