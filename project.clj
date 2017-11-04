bd4af2cb298ec997ccfe75b70885b36585dcd608
clartaq <clartaq@axaluna.com>
2017-11-04 16:42:48 -0400

    Just formatting.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cwiki "0.1.0-SNAPSHOT"
+(defproject cwiki "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.8.0"]
@@ -8,10 +8,10 @@
                  [com.vladsch.flexmark/flexmark-ext-gfm-strikethrough "0.27.0"]
                  [compojure "1.6.0"]
                  [hiccup "1.0.5"]
-                 [ring-server "0.5.0"]
                  [org.clojure/java.jdbc "0.7.3"]
                  [org.xerial/sqlite-jdbc "3.20.1"]
-                 ]
+                 [ring-server "0.5.0"]]
+
   :plugins [[lein-ring "0.12.1"]]
   :ring {:handler cwiki.handler/app
          :init    cwiki.handler/init
@@ -21,8 +21,8 @@
    :production
             {:ring
              {:open-browser? false
-              :stacktraces? false
-              :auto-reload? false}}
+              :stacktraces?  false
+              :auto-reload?  false}}
    :dev
             {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.6.2"]]}}
 
