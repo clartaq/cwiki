e0d8b05b9e8c09eb60be2891c595ac9f7c3372a9
clartaq <david@dneclark.net>
2017-11-04 16:48:01 -0400

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.cemerick/url "0.1.1"]
                  [clj-time "0.14.0"]
-                 [com.vladsch.flexmark/flexmark "0.27.0"]
-                 [com.vladsch.flexmark/flexmark-ext-gfm-strikethrough "0.27.0"]
+                 [com.vladsch.flexmark/flexmark "0.28.0"]
+                 [com.vladsch.flexmark/flexmark-ext-gfm-strikethrough "0.28.0"]
                  [compojure "1.6.0"]
                  [hiccup "1.0.5"]
                  [org.clojure/java.jdbc "0.7.3"]
@@ -24,6 +24,6 @@
               :stacktraces?  false
               :auto-reload?  false}}
    :dev
-            {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.6.2"]]}}
+            {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.6.3"]]}}
 
   :main cwiki.main)
