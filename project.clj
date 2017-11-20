857689696412ca2c8e683f1383bdc95b81a8cbaf
clartaq <david@dneclark.net>
2017-11-20 16:05:58 -0500

    Update SNAPSHOT version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cwiki "0.0.3"
+(defproject cwiki "0.0.4-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.8.0"]
