38792dd3d98fbbf14f61a989c37d11f6042ec1af
clartaq <clartaq@axaluna.com>
2017-11-25 15:28:34 -0500

    Saving WIP before refactor.

--- src/cwiki/routes/admin.clj
+++ src/cwiki/routes/admin.clj
@@ -20,19 +20,35 @@
      referer   "referer"} :multipart-params
     session               :session :as req}]
   (if (db/find-user-by-name username)
-      (layout/compose-user-already-exists-page)
+    (layout/compose-user-already-exists-page)
     (do
       (db/add-user username password user-role recovery-email)
       (if referer
         (redirect referer)
         (redirect "/Front Page")))))
 
+(defn- get-delete-user
+  [req]
+  (layout/view-delete-user-page req))
+
+(defn- post-delete-user
+  "Delete the user specified."
+  [{{username "user-name" referer "referer"} :multipart-params
+    session                                  :session :as req}]
+  (if-let [user-id (db/user-name->user-id username)]
+    (println "I know this user:" username ", user-id:" user-id)
+    (println "I DON'T know this user:" username))
+  (if referer
+    (redirect referer)
+    (redirect "/Front Page")))
+
 (defroutes admin-routes
-           (GET "/compact" [] (not-yet "compact"))
+           (GET "/compress" [] (not-yet "compress"))
            (GET "/backup" [] (not-yet "backup"))
            (GET "/restore" [] (not-yet "restore"))
            (GET "/create-user" request (get-create-user request))
            (POST "/create-user" [] post-create-user)
-           (GET "/edit-user" [] (not-yet "edit-user"))
-           (GET "/delete-user" [] (not-yet "delete-user"))
+           (GET "/edit-profile" [] (not-yet "edit-profile"))
+           (GET "/delete-user" request (get-delete-user request))
+           (POST "/delete-user" [] post-delete-user)
            (GET "/reset-password" [] (not-yet "reset-password")))
