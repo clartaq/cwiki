a9af098fd450aed20b55d8e234fbe8681e8dc86f
clartaq <david@dneclark.net>
2017-12-03 15:57:23 -0500

    Fixed (dumb) bug in "New" nav bar link.

--- src/cwiki/routes/home.clj
+++ src/cwiki/routes/home.clj
@@ -11,27 +11,36 @@
   (db/find-post-by-title "Front Page"))
 
 (defn read-about-page
+  "Read the 'About' page from the database."
   []
   (db/find-post-by-title "About"))
 
 (defn save-edits
+  "Save any edits to the page back to the database."
   [page-id new-title new-content req]
   (let [actual-id (Integer. ^String page-id)]
     (db/update-page-title-and-content! actual-id new-title new-content)
     (layout/view-wiki-page (db/find-post-by-title new-title) req)))
 
 (defn save-new-page
+  "Save a new page to the database."
   [title content req]
-  (println "Enter save-new-page")
   (db/insert-new-page! title content (ri/req->user-id req))
   (layout/view-wiki-page (db/find-post-by-title title) req))
 
-(defn home [req]
+(defn home
+  "Handle a request to view the 'Home' page if there is an
+  authenticated user. Otherwise, force them to log in first."
+  [req]
   (if (ri/is-authenticated-user? req)
     (layout/view-wiki-page (read-front-page) req)
     (redirect "/login")))
 
-(defn about [req]
+(defn about
+  "Handle a request for the 'About' route if there is
+  an authenticated user for the session. Otherwise,
+  force them to log in."
+  [req]
   (if (ri/is-authenticated-user? req)
     (layout/view-wiki-page (read-about-page) req)
     (redirect "/login")))
@@ -41,12 +50,10 @@
            (GET "/about" request (about request))
            (POST "/save-edits" request
              (let [params (request :multipart-params)]
-               (println "saw post to route /save-edits")
                (save-edits (get params "page-id")
                            (get params "title")
                            (get params "content") request)))
            (POST "/save-new-page" request
              (let [params (request :multipart-params)]
-               (println "saw post to route /save-new-page")
                (save-new-page (get params "title")
                               (get params "content") request))))
