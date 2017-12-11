aa688f2016444ff52116ed2ccb8aae4633d7f929
clartaq <clartaq@axaluna.com>
2017-12-11 15:44:20 -0500

    Now when a user edits their own profile, the session identity also get changed.

--- src/cwiki/routes/admin.clj
+++ src/cwiki/routes/admin.clj
@@ -6,7 +6,7 @@
             [cwiki.layouts.admin :as admin-layout]
             [cwiki.layouts.base :as layout]
             [cwiki.models.db :as db]
-            [cwiki.util.pp :as pp]
+            ;[cwiki.util.pp :as pp]
             [cwiki.util.req-info :as ri]
             [ring.util.response :refer [redirect status]]
             [clojure.string :as s]
@@ -40,8 +40,7 @@
   exist."
   [{{username  "user-name" password "password"
      user-role "user-role" recovery-email "recovery-email"
-     referer   "referer"} :multipart-params
-    session               :session :as req}]
+     referer   "referer"} :multipart-params}]
   (if (db/find-user-by-name username)
     (admin-layout/compose-user-already-exists-page)
     (do
@@ -57,24 +56,20 @@
 (defn get-user-to-edit
   "Get an edit-profile page and return it."
   [req]
-  (println "get-user-to-edit")
   (admin-layout/select-user-to-edit-page req))
 
 (defn post-user-to-edit
   [{{username "user-name" password "password"
      referer  "referer"} :multipart-params
     session              :session :as req}]
-  (println "post-user-to-edit")
   (let [session-user (ri/req->user-name req)]
     (if (not (db/get-user-by-username-and-password session-user password))
       (build-response (admin-layout/wrong-password-page req) req 422)
       (let [user-referer {:original-referer referer}]
         (if-let [user-info (merge user-referer (db/find-user-by-name username))]
-          (do
-            (println "user-info:" (pp/pp-map user-info))
-            (let [new-session (assoc (redirect "/edit-profile")
-                                :session (assoc session :edit-user-info user-info))]
-              new-session))
+          (let [new-session (assoc (redirect "/edit-profile")
+                              :session (assoc session :edit-user-info user-info))]
+            new-session)
           ; should never happen
           (do
             (build-response (admin-layout/cannot-find-user req) req 500)
@@ -84,7 +79,6 @@
 
 (defn get-edit-profile
   [req]
-  (println "get-edit-profile")
   (admin-layout/edit-user-page req))
 
 (defn proposed-name-same-as-existing-name?
@@ -133,47 +127,56 @@
   [{{new-name "new-user-name" new-password "new-password"
      new-role "new-role" new-email "new-email"} :multipart-params
     session                                     :session :as req}]
-  (if (not (seq new-name))
-    ; Should never happen since this is a required field in the form.
-    (build-response (admin-layout/must-not-be-empty-page) 400)
+  ; No matter how we return, we should use a modified request where the
+  ; profile information about the user being modified has been removed.
+  (let [new-session (atom (dissoc session :edit-user-info))
+        new-req (assoc req :session @new-session)]
+    (if (not (seq new-name))
+      ; Should never happen since this is a required field in the form.
+      (build-response (admin-layout/must-not-be-empty-page) new-req 400)
+      ; Normal code path.
+      (let [changes (atom {})
+            existing-user-info (:edit-user-info session)
+            existing-user-id (:user_id existing-user-info)
+            logged-in-user (get-in session [:identity :user_name])
+            old-name (:user_name existing-user-info)
+            same-as-logged-in-user (= old-name logged-in-user)
+            old-role (s/replace-first
+                       (:user_role existing-user-info)
+                       ":" "")
+            old-email (:user_email existing-user-info)
+            old-password (:user_password existing-user-info)
+            original-referer (:original-referer existing-user-info)]
 
-    (let [changes (atom {})
-          existing-user-info (:edit-user-info session)
-          existing-user-id (:user_id existing-user-info)
-          old-name (:user_name existing-user-info)
-          old-role (s/replace-first
-                     (:user_role existing-user-info)
-                     ":" "")
-          old-email (:user_email existing-user-info)
-          old-password (:user_password existing-user-info)
-          original-referer (:original-referer existing-user-info)]
-
-      ; Run the functions to check the new settings against the rules.
-      (if (and (not= old-name new-name)
-               (proposed-name-same-as-existing-name? new-name))
-        (build-response (admin-layout/compose-user-already-exists-page) 409)
-        (do
-          (when (not= old-name new-name)
-            (swap! changes conj {:user_name new-name}))
-          (when-let [new-user-role (get-new-role old-role new-role)]
-            (swap! changes conj {:user_role new-user-role}))
-          (when-let [new-user-email (get-new-email old-email new-email)]
-            (swap! changes conj {:user_email new-user-email}))
-          (when-let [new-user-password (get-new-password old-password new-password)]
-            (swap! changes conj {:user_password new-user-password}))
-          (when (not (empty? @changes))
-            (swap! changes conj {:user_touched (c/to-sql-time (t/now))}))
-          (when (not (empty? @changes))
-            (let [cleaned-existing (dissoc existing-user-info :original-referer)
-                  merged-changes (merge cleaned-existing @changes)]
-              (db/update-user existing-user-id merged-changes)))
-          (let [redir (if original-referer
-                        original-referer
-                        "/Front Page")
-                cleaned-session (dissoc session :edit-user-info)
-                new-session (assoc (redirect redir)
-                              :session cleaned-session)]
-            new-session))))))
+        ; Run the functions to check the new settings against the rules.
+        (if (and (not= old-name new-name)
+                 (proposed-name-same-as-existing-name? new-name))
+          (build-response (admin-layout/compose-user-already-exists-page) new-req 409)
+          (do
+            (when (not= old-name new-name)
+              (swap! changes conj {:user_name new-name}))
+            (when-let [new-user-role (get-new-role old-role new-role)]
+              (swap! changes conj {:user_role new-user-role}))
+            (when-let [new-user-email (get-new-email old-email new-email)]
+              (swap! changes conj {:user_email new-user-email}))
+            (when-let [new-user-password (get-new-password old-password new-password)]
+              (swap! changes conj {:user_password new-user-password}))
+            (when (not (empty? @changes))
+              (swap! changes conj {:user_touched (c/to-sql-time (t/now))})
+              (let [cleaned-existing (dissoc existing-user-info :original-referer)
+                    merged-changes (merge cleaned-existing @changes)]
+                (db/update-user existing-user-id merged-changes)
+                ; If the user is updating their own profile, update the session too.
+                (when same-as-logged-in-user
+                  (swap! new-session
+                         assoc :identity (dissoc merged-changes :user_password)))))
+            (let [redir (if original-referer
+                          original-referer
+                          "/Front Page")
+                  _ (println "redir" redir)
+                  newer-session (assoc (redirect redir)
+                                  :session @new-session)]
+              newer-session)))))))                          ; ))
 
 ;;
 ;; Functions relating to deleting a user.
@@ -188,8 +191,7 @@
   "Delete the user specified."
   [{{username "user-name"
      password "password"
-     referer  "referer"} :multipart-params
-    session              :session :as req}]
+     referer  "referer"} :multipart-params :as req}]
   (let [session-user (ri/req->user-name req)]
     (if (not (db/get-user-by-username-and-password session-user password))
       (build-response (admin-layout/wrong-password-page req) req 422)
