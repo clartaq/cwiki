c78fa9f8bbdc67160bd3d8998cb17dc7fb0473f9
clartaq <clartaq@axaluna.com>
2017-12-21 17:52:45 -0500

    Additional/revised tests.

--- test/cwiki/test/util/wikilinks.clj
+++ test/cwiki/test/util/wikilinks.clj
@@ -47,34 +47,80 @@
 
 (deftest link-parts->html-link-test
   (testing "link-parts->html-link"
-    (println "about to do first test")
-    (is (= (link-parts->html-link {:title-part "Front Page"
+    (is (= (link-parts->html-link {:title-part   "Front Page"
                                    :display-part "Front Page"} nil)
-           "<a href=\"Front%20Page\" style=\"any-old-style\">Front Page</a>"))
-    (println "about to do second test")
-    (is (= (link-parts->html-link {:title-part "Other Wiki Software"
+           (str "<a href=\"Front%20Page\" style=\"" ok-to-link-style
+                "\">Front Page</a>")))
+    (is (= (link-parts->html-link {:title-part   "Other Wiki Software"
                                    :display-part "Other Wiki Software"} nil)
-           "<a href=\"Other%20Wiki%20Software\" style=\"any-old-style\">Other Wiki Software</a>"))
-    (println "about to do third test")
-    (is (= (link-parts->html-link {:title-part "Some Random Page"
+           (str "<a href=\"Other%20Wiki%20Software\" style=\"" ok-to-link-style
+                "\">Other Wiki Software</a>")))
+    (is (= (link-parts->html-link {:title-part   "Some Random Page"
                                    :display-part "Some Random Page"} nil)
-           "<a href=\"Some%20Random%20Page\" style=\"color:red\">Some Random Page</a>"))
-    (is (= (link-parts->html-link {:title-part "Some Random Title"
+           (str "<a href=\"Some%20Random%20Page\" style=\""
+                non-existent-link-style "\">Some Random Page</a>")))
+    (is (= (link-parts->html-link {:title-part   "Some Random Title"
                                    :display-part "Some Random Display"} nil)
-           "<a href=\"Some%20Random%20Title\" style=\"color:red\">Some Random Display</a>"))
-    (is (= (link-parts->html-link {:title-part "Front Page"
+           (str "<a href=\"Some%20Random%20Title\" style=\""
+                non-existent-link-style "\">Some Random Display</a>")))
+    (is (= (link-parts->html-link {:title-part   "Front Page"
                                    :display-part "Root Page"} nil)
-           "<a href=\"Front%20Page\" style=\"any-old-style\">Root Page</a>"))))
+           (str "<a href=\"Front%20Page\" style=\"" ok-to-link-style
+                "\">Root Page</a>")))))
 
 (deftest replace-wikilinks-test
   (testing "replace-wikilinks"
     (is (= "" (replace-wikilinks "" nil)))
     (is (= "No links here" (replace-wikilinks "No links here" nil)))
-    (is (= "<a href=\"only%20a%20link\" style=\"color:red\">only a link</a>"
+    (is (= (str "<a href=\"only%20a%20link\" style=\"" non-existent-link-style
+                "\">only a link</a>")
            (replace-wikilinks "[[only a link]]" nil)))
-    (is (= "The <a href=\"Front%20Page\" style=\"any-old-style\">Front Page</a> and a <a href=\"non-existent\" style=\"color:red\">non-existent</a> post"
+    (is (= (str "The <a href=\"Front%20Page\" style=\"" ok-to-link-style
+                "\">Front Page</a> and a <a href=\"non-existent\" style=\""
+                non-existent-link-style "\">non-existent</a> post")
            (replace-wikilinks "The [[Front Page]] and a [[non-existent]] post" nil)))
-    (is (= "The <a href=\"Front%20Page\" style=\"any-old-style\">Root Page</a> and a <a href=\"non-existent\" style=\"color:red\">non-existent</a> post"
+    (is (= (str "The <a href=\"Front%20Page\" style=\"" ok-to-link-style
+                "\">Root Page</a> and a <a href=\"non-existent\" style=\""
+                non-existent-link-style "\">non-existent</a> post")
            (replace-wikilinks "The [[Front Page|Root Page]] and a [[non-existent]] post" nil)))))
 
+;;
+;; Some data and tests to assure that the link replacer applies the correct
+;; special styles for admins and readers when needed.
+;;
 
+(def fake-admin-user-session
+  {:session {:identity {:user_role ":admin"}}})
+
+(def fake-reader-user-session
+  {:session {:identity {:user_role ":reader"}}})
+
+(deftest replace-links-for-admin-test
+  (testing "wikilink replacement for admin pages."
+    ; Link to a non-existent page should use the "non-existent-link-style".
+    (is (= (str "<a href=\"only%20a%20link\" style=\"" non-existent-link-style
+                "\">only a link</a>")
+           (replace-wikilinks "[[only a link]]" fake-admin-user-session)))
+    ; Link to an existing page should use the "ok-to-link-style".
+    (is (= (str "The <a href=\"Front%20Page\" style=\"" ok-to-link-style
+                "\">Front Page</a> is here.")
+           (replace-wikilinks "The [[Front Page]] is here."
+                              fake-admin-user-session)))
+    ; Link to an admin page should use "ok-to-link-style" when user is admin.
+    (is (= (str "<a href=\"Admin\" style=\"" ok-to-link-style "\">Admin</a>")
+           (replace-wikilinks "[[Admin]]" fake-admin-user-session)))))
+
+(deftest replace-links-for-reader-test
+  (testing "wikilink replacement for user with reader role"
+    ; Link to admin page should be disabled.
+    (is (= (str "<a href=\"Admin\" style=\"" disabled-link-style "\">Admin</a>")
+           (replace-wikilinks "[[Admin]]" fake-reader-user-session)))
+    ; Link to existing page should use normal link.
+    (is (= (str "The <a href=\"Front%20Page\" style=\"" ok-to-link-style
+                "\">Front Page</a> is here.")
+           (replace-wikilinks "The [[Front Page]] is here."
+                              fake-reader-user-session)))
+    ; Link to non-existent page should be disabled.
+    (is (= (str "<a href=\"only%20a%20link\" style=\"" disabled-link-style
+                "\">only a link</a>")
+           (replace-wikilinks "[[only a link]]" fake-reader-user-session)))))
