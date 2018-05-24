bdb902910d867024ac4517f0d53f2e90834e0eeb
david <david@localhost>
2018-05-24 14:51:28 -0400

    Starting to incorporate layout from main program.

--- src/clj/cwiki/layouts/editor.clj
+++ src/clj/cwiki/layouts/editor.clj
@@ -62,10 +62,20 @@
   [new-content]
   (reset! editable-content new-content))
 
+(defn sidebar-and-editor
+  "Return a sidebar and div with the given content. This is just like the
+  `sidebar-and-article` in the base layout file except that the content
+  of the main section is not restricted in width."
+  [sidebar article]
+  [:div {:class "sidebar-and-article"}
+   sidebar
+   article])
+
 (defn layout-editor-page
   "Create the base page layout and plug the content into it."
   [post-map req]
   (reset! post-map-for-editing post-map)
+  (debugf "(get-post-map-for-editing): %s" (get-post-map-for-editing))
   (reset! editable-content (db/page-map->content @post-map-for-editing))
   (let [id (db/page-map->id @post-map-for-editing)
         title (db/page-map->title @post-map-for-editing)
@@ -77,34 +87,100 @@
       {:ng-app "CWiki" :lang "en"}
       (standard-head (get-post-map-for-editing))
       [:body {:class "page-container"}
-       [:header {:class "test-header"}
-        [:h1 "MDE Test"]
-        [:p "This program provides a test fixture for the MDE Markdown editor."]]
-       [:main {:class "main-area"}
-        [:aside {:class "sidebar"}
-         [:p "This is a sidebar. Just here because I want the test page to
-       look similar to the page where I intend to use the editor."]]
-        [:section {:class "editor-section"}
-         [:div {:class "title-edit-section"}
-          [:p "Title Edit Section"]]
-         [:div {:class "tag-edit-secton"}
-          [:p "Tag Edit Section"]]
-         [:div {:id "editor-container" :class "editor-container"}
-          ; This should get overwritten by the running ClojureScript editor.
-          [:p "The stuff from the ClojureScript editor should show up here."]]
-         ;[:div {:class "button-bar-container"}
-         ; [:input {:type    "button"
-         ;          :id      "Save Button"
-         ;          :name    "save-button"
-         ;          :value   "Save Changes"
-         ;          :class   "form-button button-bar-item"
-         ;          :onclick "console.log(\"Saw click on save button!\");"}]
-         ; [:input {:type    "button"
-         ;          :id      "Cancel Button"
-         ;          :name    "cancel-button"
-         ;          :value   "Cancel"
-         ;          :class   "form-button button-bar-item"
-         ;          :onclick "window.history.back();"}]]
-         ]]
-       (base/footer-component)]
-      (standard-end-of-body))))
+       (base/wiki-header-component post-map req {:editing true})
+       (sidebar-and-editor
+         (base/sidebar-aside req)
+         [:section {:class "editor-section"}
+          [:div {:class "title-edit-section"}
+           [:p "Title Edit Section"]]
+          [:div {:class "tag-edit-secton"}
+           [:p "Tag Edit Section"]]
+          [:div {:id "editor-container" :class "editor-container"}
+           ; This should get overwritten by the running ClojureScript editor.
+           [:p "The stuff from the ClojureScript editor should show up here."]]
+          ;[:div {:class "button-bar-container"}
+          ; [:input {:type    "button"
+          ;          :id      "Save Button"
+          ;          :name    "save-button"
+          ;          :value   "Save Changes"
+          ;          :class   "form-button button-bar-item"
+          ;          :onclick "console.log(\"Saw click on save button!\");"}]
+          ; [:input {:type    "button"
+          ;          :id      "Cancel Button"
+          ;          :name    "cancel-button"
+          ;          :value   "Cancel"
+          ;          :class   "form-button button-bar-item"
+          ;          :onclick "window.history.back();"}]]
+          ])
+       (base/footer-component)
+       (standard-end-of-body)])))
+
+#_(defn- tag-editor-list-component
+    [tags]
+    (let [tv (vec tags)]
+      [:div {:class "tag-edit-container"}
+       [:label {:class "tag-edit-label"} "Tags"]
+       [:div {:class "tag-edit-tag-list"}
+        (text-field {:class "tag-text-field"} "tag0" (nth tv 0 ""))
+        (text-field {:class "tag-text-field"} "tag1" (nth tv 1 ""))
+        (text-field {:class "tag-text-field"} "tag2" (nth tv 2 ""))
+        (text-field {:class "tag-text-field"} "tag3" (nth tv 3 ""))
+        (text-field {:class "tag-text-field"} "tag4" (nth tv 4 ""))
+        (text-field {:class "tag-text-field"} "tag5" (nth tv 5 ""))
+        (text-field {:class "tag-text-field"} "tag6" (nth tv 6 ""))
+        (text-field {:class "tag-text-field"} "tag7" (nth tv 7 ""))
+        (text-field {:class "tag-text-field"} "tag8" (nth tv 8 ""))
+        (text-field {:class "tag-text-field"} "tag9" (nth tv 9 ""))]]))
+
+#_(defn compose-create-or-edit-page
+    "Will compose a page to create or edit a page in the wiki. The
+    difference is based on whether or not the post-map passed as
+    argument has a nil entry for the :post_id key in the map -- nil causes
+    creation, non-nil is an edit."
+    [post-map req]
+    (let [id (db/page-map->id post-map)
+          title (db/page-map->title post-map)
+          content (db/page-map->content post-map)
+          tags (db/get-tag-names-for-page id)]
+      (html5
+        {:lang "en"}
+        (standard-head post-map)
+        [:body {:class "page"}
+         (wiki-header-component post-map req {:editing true})
+         (sidebar-and-article
+           (sidebar-aside req)
+           [:div {:class "editor-container"}
+            (form-to {:enctype "multipart/form-data" :class "editor-form"}
+                     (if id
+                       [:post "save-edits"]
+                       [:post "save-new-page"])
+                     (when id
+                       (hidden-field :page-id id))
+                     [:div {:class "form-group"}
+                      [:div {:class "form-label-div"}
+                       [:label {:class "form-label required"
+                                :for   "title"} "Page Title"]]
+                      (text-field {:class     "form-title-field"
+                                   :autofocus "autofocus"} "title" title)]
+                     (tag-editor-list-component tags)
+                     ; KEEP THIS FOR NOW
+                     ;[:div {:class "form-group"}
+                     ; [:div {:class "form-label-div"}
+                     ;  [:label {:class "form-label"
+                     ;           :for   "tags"} "Tags"]]
+                     ; [:input {:type "submit" :id "new-tag-button"}]]
+                     [:div {:class "form-group"}
+                      [:div {:class "form-label-div"}
+                       [:label {:class "form-label"
+                                :for   "content"} "Page Content"]]
+                      (text-area {:class "form-text-area"} "content" content)]
+                     [:div {:class "button-bar-container"}
+                      (submit-button {:id    "Save Button"
+                                      :class "form-button button-bar-item"}
+                                     "Save Changes")
+                      [:input {:type    "button" :name "cancel-button"
+                               :value   "Cancel"
+                               :class   "form-button button-bar-item"
+                               :onclick "window.history.back();"}]])])
+         (footer-component)])))
+
