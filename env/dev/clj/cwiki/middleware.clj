e4753ec7a64fd06acbde5b1bd5dec51f0a19c66d
clartaq <clartaq@axaluna.com>
2018-09-26 17:57:04 -0400

    Remove deprecated function.

--- env/dev/clj/cwiki/middleware.clj
+++ env/dev/clj/cwiki/middleware.clj
@@ -4,7 +4,6 @@
             [prone.middleware :refer [wrap-exceptions]]
             [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
             [ring.middleware.file :refer [wrap-file]]
-            [ring.middleware.file-info :refer [wrap-file-info]]
             [ring.middleware.reload :refer [wrap-reload]]))
 
 (println "wrapping dev middleware")
@@ -18,6 +17,5 @@
       ; Makes static assets in $PROJECT_DIR/resources/public/ available.
       (wrap-file "resources")
       ; Content-Type, Content-Length, and Last Modified headers for files in body.
-      (wrap-file-info)
       (wrap-exceptions)
       (wrap-reload)))
