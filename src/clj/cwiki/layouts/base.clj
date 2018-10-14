9378259e58dc96425166c7278081e5334797605c
David_Clark <clartaq@axaluna.com>
2018-10-14 11:04:57 -0400

    Added a favicon for various browsers.

--- src/clj/cwiki/layouts/base.clj
+++ src/clj/cwiki/layouts/base.clj
@@ -118,6 +118,15 @@
   [post-map which-highlighter]
     [:head
      [:title (get-tab-title post-map)]
+     [:link {:rel "shortcut icon" :href "/img/favicon/favicon.ico"}]
+     [:link {:rel "apple-touch-icon" :sizes "180x180" :href "/img/favicon/apple-touch-icon.png"}]
+     [:link {:rel "icon" :type "image/png" :sizes "32x32" :href "/img/favicon/favicon-32x32.png"}]
+     [:link {:rel "icon" :type "image/png" :sizes "16x16" :href "/img/favicon/favicon-16x16.png"}]
+     [:link {:rel "manifest" :href "/img/favicon/site.webmanifest"}]
+     [:link {:rel "mask-icon" :href "/img/favicon/safari-pinned-tab.svg" :color "#5bbad5"}]
+     [:meta {:name "msapplication-TileColor" :content "#da532c"}]
+     [:meta {:name "msapplication-config" :content "/img/favicon/browserconfig.xml"}]
+     [:meta {:name "theme-color" :content "#ffffff"}]
      (include-css (get-css-path))
      (when (= which-highlighter :editor-highlighter)
        (include-css "//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/styles/default.min.css"))
