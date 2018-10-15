1b2d92a4f539004e4497c9aabb92fb36a4f394de
clartaq <clartaq@axaluna.com>
2018-10-15 14:26:58 -0400

    Reworking Reactivity in editor. WIP.

--- src/cljs/cwiki_mde/core.cljs
+++ src/cljs/cwiki_mde/core.cljs
@@ -23,12 +23,26 @@
   [doc-save-fn delay-ms page-map-atom]
   (reset! delay-handle (.setTimeout js/window doc-save-fn delay-ms page-map-atom)))
 
+(defn restart-autosave-delay!
+  [the-save-fn delay-ms]
+  (reset! delay-handle (.setTimeout js/window the-save-fn delay-ms)))
+
 (defn change-watcher!
   [doc-save-fn page-map-atom]
   (let [delay (* 1000 (get-in @page-map-atom [:options :editor_autosave_interval]))]
     (when (pos? delay)
       (clear-autosave-delay!)
       (start-autosave-delay! doc-save-fn delay page-map-atom))))
+
+(defn notify-autosave
+  [options]
+  (println "autosave notified")
+  (let [delay (* 1000 (:editor_autosave_interval options))
+        the-save-fn (:assemble-and-save-fn options)]
+    (println "the-save-fn: " the-save-fn)
+    (when (pos? delay)
+      (clear-autosave-delay!)
+      (restart-autosave-delay! the-save-fn delay))))
 
 ;-------------------------------------------------------------------------------
 ; The editor components.
@@ -79,8 +93,27 @@
                     (change-watcher! doc-save-function page-map-atom)
                     (when (get-in @page-map-atom [:options :send-every-keystroke])
                       (ws/send-message! [:hey-server/content-updated
-                                         {:?data new-content}]))
+                                         {:data new-content}]))
                     new-content))}]])
+
+(defn layout-editor-pane
+  [content-atom options]
+  (trace "Enter layout-editor-pane.")
+  (fn [content-atom]
+    [:div {:class "editor-container"}
+     [:textarea
+      {:class     "editor-textarea"
+       :value     @content-atom
+       :on-change (fn [arg]
+                    (let [new-content (-> arg .-target .-value)]
+                      (println "new-content: " new-content)
+                      (reset! content-atom new-content)
+                      (println "updated @content-atom after reset!: " @content-atom)
+                      (notify-autosave options)
+                      (when (:send-every-keystroke options)
+                        (ws/send-message! [:hey-server/saw-a-keystroke-in-content
+                                           {:data new-content}]))
+                      new-content))}]]))
 
 (defn preview
   "The preview div."
@@ -88,6 +121,13 @@
   [:div {:class "mde-preview-class" :id "mde-preview-id"}
    (when-let [content (:page_content @the-page-map-atom)]
      (markdown-component content))])
+
+(defn layout-preview-pane
+  [content-atom]
+  (fn [content-atom]
+    [:div {:class "mde-preview-class" :id "mde-preview-id"}
+     (when @content-atom
+       (markdown-component @content-atom))]))
 
 ;;
 ;; Websocket messages handlers to work with the server.
@@ -99,6 +139,12 @@
   "Send a message to the server to save the document."
   [page-map-atom]
   (ws/send-message! [:hey-server/save-doc {:data @page-map-atom}]))
+
+(defn doc-save-fn
+  "Send a message to the server to save the document."
+  [page-map]
+  (println "doc-save-fn: page-map: " page-map)
+  (ws/send-message! [:hey-server/save-doc {:data page-map}]))
 
 (defn editor-handshake-handler
   "Handle the handshake event between the server and client. This function
@@ -150,7 +196,7 @@
         (swap! page-map-atom assoc-in [:tags n] new-tag))
       (when (get-in @page-map-atom [:options :send-every-keystroke])
         (ws/send-message! [:hey-server/content-updated
-                           {:?data @page-map-atom}])))))
+                           {:data @page-map-atom}])))))
 
 (defn make-tag-input-element
   "Return an input element for the nth tag from the vector."
@@ -198,7 +244,7 @@
                                    (swap! page-map-atom assoc :page_title new-title)
                                    (when (get-in pm [:options :send-every-keystroke])
                                      (ws/send-message! [:hey-server/content-updated
-                                                        {:?data pm}]))))})]
+                                                        {:data pm}]))))})]
     [:div {:class "mde-title-edit-section"}
      [:div {:class "form-label-div"}
       [:label {:class "form-label required"
@@ -206,50 +252,219 @@
      [:input inp]]))
 
 (defn the-editor-container
-  "Starts the websocket router and returns a function that lays out
-  the editor and preview area side-by-side."
+  "Lays out the editor, including the editor heading (title, tags, etc) at the
+  top, and the editor and preview panes side-by-side."
   [page-map-atom]
-  (trace "the-editor-container")
-  (fn [page-map-atom]
-    (tracef "the-editor-container: @the-page-map: %s" @page-map-atom)
-    [:div {:class "inner-editor-container"}
+  (tracef "the-editor-container: @the-page-map: %s" @page-map-atom)
+  [:div {:class "inner-editor-container"}
 
 
-     [:header {:class "editor-header"}
-      [make-title-input-element page-map-atom]
-      [make-tag-list-input-component page-map-atom]
+   [:header {:class "editor-header"}
 
-      [:div {:class "button-bar-container"}
-       [:input {:type    "button"
-                :id      "Save Button"
-                :name    "save-button"
-                :value   "Save Changes"
-                :class   "form-button button-bar-item"
-                :onClick #(do
-                            (trace "Saw Click on Save Button!")
-                            (ws/send-message! [:hey-server/save-doc-and-quit
-                                               {:data @page-map-atom}]))}]
-       [:input {:type    "button"
-                :id      "Cancel Button"
-                :name    "cancel-button"
-                :value   "Cancel"
-                :class   "form-button button-bar-item"
-                :onClick #(do
-                            (trace "Saw Click on the Cancel Button!")
-                            (ws/send-message! [:hey-server/cancel-editing]))}]]
+    [:div {:class "button-bar-container"}
+     [:input {:type    "button"
+              :id      "Save Button"
+              :name    "save-button"
+              :value   "Save Changes"
+              :class   "form-button button-bar-item"
+              :onClick #(do
+                          (trace "Saw Click on Save Button!")
+                          (ws/send-message! [:hey-server/save-doc-and-quit
+                                             {:data @page-map-atom}]))}]
+     [:input {:type    "button"
+              :id      "Cancel Button"
+              :name    "cancel-button"
+              :value   "Cancel"
+              :class   "form-button button-bar-item"
+              :onClick #(do
+                          (trace "Saw Click on the Cancel Button!")
+                          (ws/send-message! [:hey-server/cancel-editing]))}]]
 
-      [:div {:class "mde-content-label-div"}
-       [:label {:class "form-label"
-                :for   "content"} "Page Content"]]]
+    [make-title-input-element page-map-atom]
+    [make-tag-list-input-component page-map-atom]
+    [:div {:class "mde-content-label-div"}
+     [:label {:class "form-label"
+              :for   "content"} "Page Content"]]]
 
-     [:section {:class "editor-and-preview-section"}
-      [editor page-map-atom]
-      [preview page-map-atom]]
+   [:section {:class "editor-and-preview-section"}
+    [editor page-map-atom]
+    [preview page-map-atom]]])
 
-]))
+(defn layout-button-bar
+  [options]
+  (println "Enter layout-button-bar")
+  (println "options: " options)
+  (println "re-assembler-fn: " (:re-assembler-fn options))
+  [:div {:class "button-bar-container"}
+   [:input {:type    "button"
+            :id      "Save Button"
+            :name    "save-button"
+            :value   "Save Changes"
+            :class   "form-button button-bar-item"
+            :onClick #(do
+                        (trace "Saw Click on Save Button!")
+                        (println "result of calling re-assembler-fn: "
+                                 (let [fxn (:re-assembler-fn options)]
+                                   (fxn)))
+
+                        (ws/send-message! [:hey-server/save-doc-and-quit
+                                           {:data (let [fxn (:re-assembler-fn options)
+                                                        new-page-map (fxn)]
+                                                    (println "new-page-map: " new-page-map)
+                                                    new-page-map)}]))}]
+   [:input {:type    "button"
+            :id      "Cancel Button"
+            :name    "cancel-button"
+            :value   "Cancel"
+            :class   "form-button button-bar-item"
+            :onClick #(do
+                        (trace "Saw Click on the Cancel Button!")
+                        (ws/send-message! [:hey-server/cancel-editing]))}]])
+
+(defn new-tag-change-listener
+  [tags-vector-atom n single-tag-atom options]
+  (println "tag changed: n: " n)
+  (fn [arg]
+    (let [new-tag (-> arg .-target .-value)]
+      (println "new-tag: " new-tag)
+      (notify-autosave options)
+      (if (blank? new-tag)
+        (let [old-tag-vec @tags-vector-atom
+              new-vec (vec (concat (subvec old-tag-vec 0 n)
+                                   (subvec old-tag-vec (inc n))))]
+          (println "new-vec: " new-vec)
+          (reset! tags-vector-atom new-vec))
+        (do
+          (reset! single-tag-atom new-tag)
+          (println "@single-tag-atom: " @single-tag-atom)
+          (let [new-vec (swap! tags-vector-atom assoc n new-tag)]
+            ;(swap! tags-vector-atom assoc n new-tag)
+            (println "new-vec: " new-vec)
+            new-vec)))
+      (when (:send-every-keystroke options)
+        (ws/send-message! [:hey-server/tags-updated
+                           {:data @tags-vector-atom}])))))
+
+(defn layout-tag-input-element
+  [tags-vector-atom n options]
+  (println "layout-tag-input-element: @tags-vector-atom: " @tags-vector-atom)
+  (fn [tags-vector-atom]
+    (let [tag-of-interest (reagent/atom (nth @tags-vector-atom n ""))]
+      [:input {:type        "text"
+               :class       "mde-tag-text-field"
+               :placeholder (str "Tag #" (+ 1 n))
+               :value       @tag-of-interest
+               :on-change   (new-tag-change-listener tags-vector-atom n
+                                                     tag-of-interest options)}])))
+
+(defn layout-tags-editor
+  [tags-atom options]
+  (println "Enter layout-tags-editor")
+  (println "options: " options)
+  (println "tags-atom: " tags-atom)
+  (println "@tags-atom: " @tags-atom)
+  [:section {:class "tag-edit-container tag-edition-section"}
+   [:label {:class "tag-edit-label"} "Tags"]
+   [:div {:class "mde-tag-edit-list" :id "mde-tag-edit-list"}
+    (for [n (range 10)]
+      ^{:key (str "tag-" n)} [layout-tag-input-element tags-atom n options])]])
+
+(defn layout-title-editor
+  "Layout the title editing control and return the layout."
+  [title-atom options]
+  (println "layout-title-editor: title-atom: " title-atom)
+  (println "@title-atom: " @title-atom)
+  (let [ro (when (= "Front Page" @title-atom)
+             {:readOnly "readOnly"})
+        inp (merge ro
+                   {:type      "text"
+                    :class     "mde-form-title-field"
+                    :name      "page-title"
+                    :value     (if-let [title @title-atom]
+                                 (do
+                                   (tracef "make-title-input-element: title: %s" title)
+                                   (when (= title "favicon.ico")
+                                     (info "Saw funky title request for favicon.icl"))
+                                   title)
+                                 "Enter a Title for the Page")
+                    :on-change (fn [arg] (let [new-title (-> arg .-target .-value)]
+                                           (println "new-title: " new-title)
+                                           (notify-autosave options)
+                                           (reset! title-atom new-title)
+                                           (println "new @title-atom: " @title-atom)
+                                           (when (:send-every-keystroke options)
+                                             (ws/send-message! [:hey-server/title-updated
+                                                                {:data new-title}]))))})]
+    (println "@title-atom in function: " @title-atom)
+    [:div {:class "mde-title-edit-section"}
+     [:div {:class "form-label-div"}
+      [:label {:class "form-label required"
+               :for   "page-title"} "Page Title"]]
+     [:input inp]]))
+
+(defn layout-editor-header
+  [title-atom tags-atom options]
+  (println "layout-editor-header")
+  [:header {:class "editor-header"}
+   [layout-button-bar options]
+   [layout-title-editor title-atom options]
+   [layout-tags-editor tags-atom options]
+   [:div {:class "mde-content-label-div"}
+    [:label {:class "form-label"
+             :for   "content"} "Page Content"]]])
+
+(defn layout-editor-and-preview-section
+  [content-atom options]
+  (println "layout-editor-and-preview-section")
+  [:section {:class "editor-and-preview-section"}
+   [layout-editor-pane content-atom options]
+   [layout-preview-pane content-atom]])
+
+(defn layout-inner-editor-container
+  "Lays out the section of the wiki page containing the editor, including the
+  heading (title, tags, etc.) at the top, and the editor and preview panes
+  side-by-side at the bottom. Returns the layout."
+  [page-map-atom]
+  (tracef "layout-inner-editor-container: @page-map-atom: " @page-map-atom)
+  (println "layout-inner-editor-container")
+  (when @page-map-atom
+    (let [pm @page-map-atom
+          title-atom (reagent/atom (:page_title pm))
+          ; The tags are Reactive, but NOT the tag vector.
+          tags-atom (atom (:tags pm))
+          content-atom (reagent/atom (:page_content pm))
+          options (:options pm)]
+      (println "    after let")
+      (letfn [(re-assembler-fn []
+                (println "Enter re-assembler-fn")
+                (println "@title-atom in re-assembler: " @title-atom)
+                (let [re-assembled-page-map (merge @page-map-atom
+                                                   {:page_title   @title-atom
+                                                    :tags         @tags-atom
+                                                    :page_content @content-atom})]
+                  (println "re-assembled-page-map: " re-assembled-page-map)
+                  re-assembled-page-map))
+              (assemble-and-save-fn []
+                (let [the-doc (re-assembler-fn)
+                      sf doc-save-fn]
+                  (println "In assemble-and-save-fn")
+                  (println "    the-doc: " the-doc)
+                  (println "    sf: " sf)
+                  (sf the-doc)))]
+        (println "    after letfn")
+        (let [final-options (merge {:re-assembler-fn      re-assembler-fn
+                                    :doc-save-fn          doc-save-fn
+                                    :assemble-and-save-fn assemble-and-save-fn}
+                                   options)]
+
+          (println "after final-options")
+          [:div {:class "inner-editor-container"}
+           [layout-editor-header title-atom tags-atom final-options]
+           [layout-editor-and-preview-section content-atom final-options]])))))
 
 (defn reload []
-  (reagent/render [the-editor-container glbl-page-map] (.getElementById js/document "outer-editor-container")))
+  (reagent/render [layout-inner-editor-container            ; the-editor-container
+                   glbl-page-map] (.getElementById js/document "outer-editor-container")))
 
 (defn ^:export main []
   (reload))
