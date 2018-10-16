1c24062b4e8af1598da3008da5221ef9118f2dfc
David_Clark <clartaq@axaluna.com>
2018-10-16 15:37:12 -0400

    Updated for 0.1.1.

--- src/clj/cwiki/layouts/base.clj
+++ src/clj/cwiki/layouts/base.clj
@@ -34,7 +34,7 @@
            (java.net URL URLDecoder)
            (java.util ArrayList)))
 
-(def program-name-and-version "CWiki v0.1.1-SNAPSHOT")
+(def program-name-and-version "CWiki v0.1.1")
 
 ;;------------------------------------------------------------------------------
 ;; Markdown translation functions.
