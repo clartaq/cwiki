26379b34237708c7567fe0e9f21e19654d7dd33b
David_Clark <clartaq@axaluna.com>
2018-12-01 14:14:32 -0500

    Need to stop taking this out. 'Cause then I just have to put it back in.

--- env/prod/clj/cwiki/middleware.clj
+++ env/prod/clj/cwiki/middleware.clj
@@ -1,7 +1,7 @@
 (ns cwiki.middleware
   (:require [buddy.auth.backends :as backends]
             [buddy.auth.middleware :refer [wrap-authentication]]
-            ;[clojure.java.io :as io]
+            [clojure.java.io :as io]
             [ring.middleware.file :refer [wrap-file]]
             [ring.middleware.file-info :refer [wrap-file-info]]
             [ring.middleware.content-type :refer [wrap-content-type]]
@@ -14,7 +14,7 @@
   ; Have to have this for the uberjar to start on a virgin system.
   ; Just makes sure the "resources" directory exists so that the
   ; "wrap-file" middleware below doesn't blow up.
-  ;(io/make-parents "resources/random-file.txt")
+  (io/make-parents "resources/random-file.txt")
   (-> handler
       (wrap-authentication (backends/session))
       (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))
