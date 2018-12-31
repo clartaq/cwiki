fa53b4e3c57ad1deb2fd3afb9a8a969710e53f8d
David_Clark <clartaq@axaluna.com>
2018-12-31 15:22:54 -0500

    Adding keyboard shortcuts and moving editor commands into their own namespace.

--- src/cljs/cwiki_mde/editor_commands.cljs
+++ src/cljs/cwiki_mde/editor_commands.cljs
@@ -1,3 +1,8 @@
+;;;;
+;;;; This namespace contains function that execute various commands on the
+;;;; editor.
+;;;;
+
 (ns cwiki-mde.editor-commands
   (:require [cljs-time.core :as t]
             [cljs-time.format :as f]
@@ -56,11 +61,20 @@
     (when (= id (:editor-textarea-id editor-state))
       (:page-content-ratom editor-state))))
 
+;-------------------------------------------------------------------------------
+; Commands
+;
+
 (defn save-page-cmd
+  "Save the page contents in the editor to the server."
   [editor-state]
   (let [save-fxn (:assemble-and-save-fn editor-state)]
     (save-fxn editor-state)))
 
+;; This function inserts text using a method determined by the browser it is
+;; running in. If `execCommand` can be used, it is, thus preserving the undo
+;; stack. Since FireFox does not support it correctly at the moment, a
+;; different method is used, but it breaks the undo stack.
 (defn insert-text-cmd
   "Inserts the text into the element wherever the cursor happens to be."
   [ele txt input-atom editor-state]
@@ -71,14 +85,13 @@
           val (.-value ele)
           before (.substring val 0 start)
           after (.substring val end (.-length val))]
-      (println "Firefox insert")
       (reset! input-atom (str before txt after)))
     (when txt
-      (println "Not Firefox")
       (.execCommand js/document "insertText" false txt)))
   ((:dirty-editor-notifier editor-state) editor-state))
 
 (defn insert-time-stamp
+  "Insert a timestamp into the input component."
   [ele editor-state]
   (let [
         formatted-now (get-formatted-now)
@@ -90,11 +103,18 @@
                      editor-state)))
 
 (defn percent-encode-selection
+  "Replace the hightlighted text in the input component with a percent
+  encoded version."
   [ele editor-state]
   (let [input-atom ele->input-atom
         start (.-selectionStart ele)
         end (.-selectionEnd ele)
         highlighted (.substr (.-value ele) start (- end start))
         encoded (percent-encode highlighted)]
-    (println "encoded: " encoded)
     (insert-text-cmd ele encoded input-atom editor-state)))
+
+(defn quit-editor-cmd
+  "Quit the editor."
+  [editor-state]
+  (let [quit-fn (:quit-fn editor-state)]
+    (quit-fn editor-state)))
