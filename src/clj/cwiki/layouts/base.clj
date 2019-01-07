a8bef398d6796d17f969b06519d8686c73f6a32f
David_Clark <clartaq@axaluna.com>
2019-01-07 16:36:10 -0500

    Start development of 0.1.5-SNAPSHOT.

--- src/clj/cwiki/layouts/base.clj
+++ src/clj/cwiki/layouts/base.clj
@@ -34,7 +34,7 @@
            (java.net URL URLDecoder)
            (java.util ArrayList)))
 
-(def program-name-and-version "CWiki v0.1.4")
+(def program-name-and-version "CWiki v0.1.5-SNAPSHOT")
 
 ;;------------------------------------------------------------------------------
 ;; Markdown translation functions.
