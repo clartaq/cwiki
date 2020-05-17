8b36085e3b4d8a64de150fbbcdeb48a43c0bde4d
clartaq <david@axaluna.com>
2020-05-17 17:50:41 -0400

    Added check for Chromium-based browsers before install devtools.

--- env/dev/cljs/cwiki/dev.cljs
+++ env/dev/cljs/cwiki/dev.cljs
@@ -3,7 +3,19 @@
     [cwiki-mde.core :as core]
     [devtools.core :as devtools]))
 
-(devtools/install!)
+;; Determine if running on a chrome-based browser. Based on
+;; Jonathan Marzullo's answer to this answer:
+;; https://stackoverflow.com/questions/4565112/javascript-how-to-find-out-if-the-user-browser-is-chrome/13348618#13348618
+;; and greatly simplified for my purposes.
+(defn chromium-based-browser?
+  []
+  (let [win-nav (.-navigator js/window)
+        vendor-name (.-vendor win-nav)
+        res (= vendor-name "Google Inc.")]
+    res))
+
+(when (chromium-based-browser?)
+  (devtools/install!))
 
 (enable-console-print!)
 
