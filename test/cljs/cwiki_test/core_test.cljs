5bce09da990d16c7d329a9e57ec15ed361be53e5
clartaq <david@axaluna.com>
2020-07-01 16:35:17 -0400

    Updated tests and commentary so things pass.

--- test/cljs/cwiki_test/core_test.cljs
+++ test/cljs/cwiki_test/core_test.cljs
@@ -1,11 +1,15 @@
 (ns cwiki-test.core-test
   (:require [cljs.test :refer-macros [deftest testing is]]
-            ;[cwiki-mde.core :as core]
-            ))
+            [cwiki-mde.core :as core]))
 
-(deftest fake-failing-test
-  (testing "fake failing description"
-    (is (= 1 2))))
+;; These tests don't do anything useful. They are just here to test the
+;; testing process.
+
+;; Uncomment this to see behavior with a failing test. It usually does
+;; all of the testing correctly but then crashes figwheel (I think).
+;(deftest fake-failing-test
+;  (testing "fake failing description"
+;    (is (= 1 2))))
 
 (deftest fake-passing-test
   (testing "fake passing description"
