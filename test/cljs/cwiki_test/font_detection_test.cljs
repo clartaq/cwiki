cedf1a2ccd607794de12915e3a63907ada620763
clartaq <david@axaluna.com>
2020-07-01 16:34:30 -0400

    Updated tests and expected results.

--- test/cljs/cwiki_test/font_detection_test.cljs
+++ test/cljs/cwiki_test/font_detection_test.cljs
@@ -4,12 +4,12 @@
 
 ;;; These tests are very system dependent. They may change depending on the
 ;;; operating system being used and the fonts actually installed on your
-;;; system. These happen to work for me.
+;;; system. They are also dependent on the test host page (test.html)
+;;; loading the project CSS. These happen to work for me.
 
 (deftest font-available?-test
   (testing "Correct detection of installed fonts."
-    ; Safari says this is false
-    (is (true? (ft/font-available? "Calibri")))
+    (is (false? (ft/font-available? "Calibri")))
     (is (false? (ft/font-available? "Calibri Regular")))
     (is (true? (ft/font-available? "Arial")))
     (is (false? (ft/font-available? "Boojum")))
@@ -19,10 +19,8 @@
     (is (false? (ft/font-available? "Helvetica Newish")))))
 
 (deftest font-family->font-used-test
-  (testing "Correct selection of and installed font from a font family.")
-  (is (= "Arial" (ft/font-family->font-used "Century Gothic, Muli, Segoe UI, Arial, sans-serif")))
+  (testing "Correct selection of an installed font from a font family.")
+  (is (= "Muli" (ft/font-family->font-used "Century Gothic, Muli, Segoe UI, Arial, sans-serif")))
   (is (= "Palatino" (ft/font-family->font-used "Palatino, Palatino Linotype, Palatino LT STD, Book Antiqua, Georgia, serif")))
-  ; Safari says this should be Menlo
-  (is (= "Consolas" (ft/font-family->font-used "Consolas, Ubuntu Mono, Menlo, Monaco, Lucida Console,\n    Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono,\n    Courier New, monospace, serif")))
-  ; Safari says this should be "Helvetica Neue".
-  (is (= "Calibri" (ft/font-family->font-used "Calibri, Segoe UI, Candara, Helvetica Neue,\n    Lucida Grande, Tahoma, Verdana, Helvetica, Arial, sans-serif"))))
+  (is (= "Ubuntu Mono" (ft/font-family->font-used "Consolas, Ubuntu Mono, Menlo, Monaco, Lucida Console,\n    Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono,\n    Courier New, monospace, serif")))
+  (is (= "Helvetica Neue" (ft/font-family->font-used "Calibri, Segoe UI, Candara, Helvetica Neue,\n    Lucida Grande, Tahoma, Verdana, Helvetica, Arial, sans-serif"))))
